3e024d972d2ec3730b5b402fcb6c997756353673
Santhoshkumard11 <santhoshkdhana@gmail.com>
2021-06-20 14:11:10 +0530

    added default response

--- GenerateLink/__init__.py
+++ GenerateLink/__init__.py
@@ -9,21 +9,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     global image_binary
     
-    image_name = "null"
+    image_name = "test1.png"
     if req.method == "GET":
             
         image_name = req.params.get('image_name')
+        if image_name == "null":
+            return func.HttpResponse("Thanks for checking out the api. Send a valid image name to generate the url", status_code=200)
         
     else:
         req_body = req.get_json()
-    
         image_binary = req_body.get('image_binary')
+    
 
-    if image_name == "null":
-        return func.HttpResponse("Thanks for checking out the api. Send a valid image name to generate the url", status_code=200)
-
-    else:
-        
         try:
             
             detected_url = run_link_generator(image_name)
